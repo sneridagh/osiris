cc629c3352d93e3287e7d95f36d36f1f0df94312
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-08-05 14:42:09 +0200

    Unified extensions for README and CHANGES. Updated MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = ['pyramid',
             'pyramid_debugtoolbar',
