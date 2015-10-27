c12b465eadec11b8c4bbb3d05c22ede6959a57ea
Carles Bruguera <carlesba@gmail.com>
2015-10-27 20:02:10 +0100

    Back to development: 1.5.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = ['pyramid',
             'waitress']
 
 setup(name='osiris',
-      version='1.5.5',
+      version='1.5.6.dev0',
       description='Pyramid based oAuth server',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
